fef1b12ec93b7d46a5499bfd9e030aff69bf6244
lill000 <alessandrogeneration@gmail.com>
2025-01-09 14:04:23 +0100

    Add files via upload

---
+++ generation_project/generation_project/conn_db.py
@@ -0,0 +1,8 @@
+
+DB_ENGINE = 'django.contrib.gis.db.backends.postgis'
+DB_NAME = 'generation'
+DB_USER = 'postgres'   
+DB_PASSWORD = 'postgres'
+DB_HOST = '165.22.75.145' 
+DB_PORT = '15432'
+
---
+++ generation_project/terminazioni_ottiche/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TerminazioniOtticheConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'terminazioni_ottiche'
